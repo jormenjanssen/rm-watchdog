ec98f95a59c8be54f919712ef0e4c3696f65f528
Jormen Janssen <J.janssen@riwo.eu>
2018-07-12 14:42:31 +0200

    Fix systemd build errors

--- watchdog/systemd-linux.go
+++ watchdog/systemd-linux.go
@@ -13,27 +13,29 @@ import (
 var WatchdogExecutablePath = "/usr/bin/watchdog"
 
 func getServiceWriter() (io.WriteCloser, error) {
-	return os.OpenFile("/etc/sytemd/watchdog.service", os.O_CREATE|O_RDWR|os.O_TRUNC)
+	return os.OpenFile("/etc/sytemd/watchdog.service", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
 }
 
 func installServicePlatformSpecific(unit string) error {
 
 	cmdReload := exec.Command("systemctl", "daemon-reload")
-	err := cmd.Run()
+	err := cmdReload.Run()
 	if cmdReload.ProcessState.Success() {
 		return fmt.Errorf("Failed to reload daemon")
 	}
 
 	cmdEnable := exec.Command("systemctl", "enable", unit)
-	err := cmd.Run()
+	err = cmdEnable.Run()
 	if cmdEnable.ProcessState.Success() {
 		return fmt.Errorf("Failed to install service")
 	}
 
 	cmdRestart := exec.Command("systemctl", "restart", unit)
-	err := cmd.Run()
+	err = cmdRestart.Run()
 
 	if cmdRestart.ProcessState.Success() {
-		return fmt.Error("Failed to restart service")
+		return nil
 	}
+
+	return fmt.Errorf("Unkown error while installing service")
 }
